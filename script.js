7044fdce0c48cddfb7a78a6baca7b9912539c109
Stacy Bridges <stcybrdgs@gmail.com>
2024-07-18 17:02:11 -0500

    finished infinite scroll functionality

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = []
+
+// check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++
+  // console.log(imagesLoaded)
+  if (imagesLoaded === totalImages) {
+    ready = true
+    loader.hidden = true
+    // console.log('ready = ', ready)
+  }
+}
 
 // helper function to set attributes on dom elements
 function setAttributes(element, attributes) {
@@ -12,6 +26,10 @@ function setAttributes(element, attributes) {
 
 // create elements for links and photos and add to dom
 function displayPhotos() {
+  imagesLoaded = 0
+  totalImages = photosArray.length
+  // console.log('totalImages = ', totalImages)
+
   photosArray.forEach((photo) => {
     // create href anchor for each unsplash photo
     const item = document.createElement('a')
@@ -28,6 +46,9 @@ function displayPhotos() {
       title: photo.alt_description,
     })
 
+    // event listener to check when each load completes
+    img.addEventListener('load', imageLoaded)
+
     // add href to image container
     item.appendChild(img)
     imageContainer.appendChild(item)
@@ -35,8 +56,8 @@ function displayPhotos() {
 }
 
 // Unsplash API
-const count = 10
-const apiKey = 'your_api_key_goes_here'
+const count = 30
+const apiKey = 'P5-mM7S2jYhYB2D0NzG2KPlx4I7yn0EUnVb6QhSIA80'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
 // Get photos from Unsplash API
@@ -53,7 +74,13 @@ async function getApiPhotos() {
 
 // when user scrolls near bottom of page, load more photos
 window.addEventListener('scroll', () => {
-  console.log('scroll!')
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false
+    getApiPhotos()
+  }
 })
 
 // Get photos from api on load
